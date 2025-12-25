26ad1628cc546d77a048f8e1bd344153b9be3e97
Sidd-2111 <siddharajshirke@gmail.com>
2025-12-25 23:20:04 +0530

    Part 2
    DATBASE CREATION

--- app/schema.py
+++ app/schema.py
@@ -1,5 +1,9 @@
 from pydantic import BaseModel
 
-class Postcreate(BaseModel):
+class PostCreate(BaseModel):
     title:str
     content:str
+
+class PostResponse(BaseModel): # specifies the value to be returned in our documentation
+     title:str
+     content:str
