50e8d5aacdc3af6ed4da4ca1b8c4625f629e4803
Sidd-2111 <siddharajshirke@gmail.com>
2025-12-27 23:20:36 +0530

    aded jwt authentication

--- app/db.py
+++ app/db.py
@@ -1,26 +1,33 @@
-from _collections_abc import AsyncGenerator
+from collections.abc import AsyncGenerator
 import uuid
-
 from sqlalchemy import Column , String ,Text , DateTime ,ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
 from datetime import datetime
+from fastapi_user_db import SQLAlchemyUserDatabase , SQLAlchemyBaseUserTableUUID
 
 DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 
 class Base(DeclarativeBase):
     pass
 
+class User (SQLAlchemyBaseUserTableUUID , Base):
+    POSTS= relationship ( argument :"Post", back_populates="user")       # ONE TO MANY RELATIONSHIP HAS BEEN IMPLEMENTED UNDER THE CLASS USING FOREIGN KEY AND USER AFTER NEXT CLASS
+
+
 class Post(Base):
     __tablename__ = "post"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("user.id"), nullable=false)
     caption = Column(Text)
     url = Column(String , nullable=False)
     file_type = Column(String , nullable=False)
     file_name = Column(String , nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
+
+    user= relationship ( argument:"User", back_populates="posts")
 
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
