34d35432323874b56833d6f001454b17e72b1329
Sidd-2111 <siddharajshirke@gmail.com>
2025-12-27 17:21:32 +0530

    database created successfully db.py

--- app/app.py
+++ app/app.py
@@ -1,28 +1,17 @@
-from distutils.dep_util import newer
-
-from fastapi import FastAPI
-from fastapi import HTTPException
+from fastapi import FastAPI,HTTPException,File,UploadFile,Form ,Depends
 from app.schema import PostCreate , PostResponse
-app = FastAPI()
+from app.db import create_db_and_tables , get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from contextlib import asynccontextmanager
 
-text_post ={}
-@app.post("/posts")
-def get_all_post(limit:int):
-    if limit:
-        return list(text_post.values(0)][:limit]
-    return text_posts
+@asynccontextmanager
+async def lifespan(app: FastAPI) :
+    await create_db_and_tables()
+    yield
 
-@app.get("/id")
-def get_post(id:int)->PostResponse:
-    if id not in text_post:
-        raise HTTPException(status_code=404, detail="post not found")
-    return text_post.get(id)
+app = FastAPI(lifespan=lifespan)
 
-@app.post("/posts")
-def create_post(post:PostCreate)->PostResponse:
-    new_post={"title":post.title,"content":post.content}
-    text_post[max(text_post.keys())+1]=new_post
-    return new_post
+@app post("/post")
 
 
 
